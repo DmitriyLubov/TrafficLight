ac8e55c5b250ea93b1b403ebcd10aaa0b0332eae
DmitriyLubov <snipere@list.ru>
2023-01-30 22:03:47 +1000

    Work with view settings in ViewController

--- TrafficLight/TrafficLightViewController.swift
+++ TrafficLight/TrafficLightViewController.swift
@@ -9,10 +9,41 @@ import UIKit
 
 final class TrafficLightViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var switchButton: UIButton!
+    
+    private let onLight = 1.0
+    private let offLight = 0.3
+    
+    private var radiusView: CGFloat {
+        redView.frame.width / 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        switchOff()
+        switchButton.layer.cornerRadius = 20
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        redView.layer.cornerRadius = radiusView
+        yellowView.layer.cornerRadius = radiusView
+        greenView.layer.cornerRadius = radiusView
+    }
+    
+    private func switchOff() {
+        redView.alpha = offLight
+        yellowView.alpha = offLight
+        greenView.alpha = offLight
     }
 
 
+    
 }
 
